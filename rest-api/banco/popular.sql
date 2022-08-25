680782ba8f436fefecaa3d7b9905089964e12635
fabianojp06 <fabianojp06@gmail.com>
2022-08-25 06:42:32 -0300

    bd config

---
+++ rest-api/banco/popular.sql
@@ -0,0 +1,5 @@
+INSERT INTO portfolio(descricao, detalhes)
+VALUES ('Desenvolvimento de website', 'Tecnologias JavaScript', 'NodeJs', 'Express e MySQL');
+
+INSERT INTO portfolio(descricao, detalhes)
+VALUES ('Desenvolvimento de APIs', 'Tecnologias Java', 'Spring', 'JDBC')
